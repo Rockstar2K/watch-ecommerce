72980bfdb952a1ef43a7ef19dcb7803c160309e4
rocofernandezpagella@gmail.com <46428180+Rockstar2K@users.noreply.github.com>
2024-10-10 23:49:04 +0200

    env configuration

--- src/services/config.js
+++ src/services/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Use environment variables for Firebase config
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY, // Access using VITE_ prefix
+    authDomain: "watch-ecommerce-pagella.firebaseapp.com",
+    projectId: "watch-ecommerce-pagella",
+    storageBucket: "watch-ecommerce-pagella.appspot.com",
+    messagingSenderId: "306818957341",
+    appId: "1:306818957341:web:5febca84b99754fe6d86f4"
 };
 
 // Initialize Firebase
